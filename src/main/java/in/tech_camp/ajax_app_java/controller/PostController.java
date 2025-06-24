42a19627eb6ce78009e56da990a9eb2eddfa00c3
soekawaharuki <h.soekawa@infomart.co.jp>
2025-06-24 13:38:00 +0900

    リクエストを処理する

--- src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
+++ src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
@@ -1,5 +1,7 @@
 package in.tech_camp.ajax_app_java.controller;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,12 +30,13 @@ public class PostController {
 
 
   @PostMapping("/posts")
-  public String savePost(@ModelAttribute("postForm") PostForm form){
-    System.out.println("メソッド呼び出し：" +form);
+  public ResponseEntity<PostEntity> savePost(@ModelAttribute("postForm") PostForm form){
     PostEntity post = new PostEntity();
     post.setContent(form.getContent());
     postRepository.insert(post);
-    return "redirect:/";
+    PostEntity resultPost = postRepository.findById(post.getId());
+    System.out.println(resultPost);
+    return ResponseEntity.ok(resultPost);
   }
   
 }
